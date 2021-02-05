e026e97e13de359ef95ecddb53102651a2608889
ishanmazhar <ishanmazhar2@gmail.com>
2021-02-05 22:52:19 +0600

    Component Basics

--- src/App.js
+++ src/App.js
@@ -1,10 +1,23 @@
 import React from 'react'; 
 import './App.css';
 
+// React library converts jsx to html
+// JSX
+
+
 function App() {
   return (
     <div className="App">
       <h1>Hello World</h1>
+      <Person />
+    </div>
+  );
+}
+
+function Person() {
+  return (
+    <div>
+      <h1>I am a component</h1>
     </div>
   );
 }
