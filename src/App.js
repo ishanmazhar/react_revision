b03f1f0e04007f19f4d3d11837578da862f15503
ishanmazhar <ishanmazhar2@gmail.com>
2021-02-13 08:46:35 +0600

    Changing State with JS event

--- src/App.js
+++ src/App.js
@@ -4,7 +4,6 @@ import Book from './components/Book';
 import SlideShow from './components/SlideShow/Slideshow'; 
 
 class App extends Component {
-  //State (only in class component)
   state = {
     books : [
       { bookname: "1984", writer: "George Orwell"},
@@ -13,18 +12,33 @@ class App extends Component {
     ],
     otherProp: "I am some other prop"
   }
-  // constructor() {
-  //   super(); 
-  //   this.state = {};
-  // }
+
+  changeBookState = () => {
+    //Wrong: this.state.books[0].bookname = "1974";
+    this.setState({
+      books : [
+        { bookname: "Nineteen Eighty Four", writer: "George Orwell"},
+        { bookname: "The Da Vinci Code", writer: "Dan Brown"},
+        { bookname: "Metamorphosis", writer: "Franz Kafka"}
+      ]
+    });
+  }
+
   render() {
+    console.log(this.state); 
     return (
       <div className="App">
         <h1>Book List</h1>
+        {/*this.changeBookState() triggers function when page loaded  / without any event
+        //this.changeBookState triggers with event (MUST BE USED)*/}
+        <button onClick={this.changeBookState}>Change State</button>
         {/* <SlideShow /> */}
-        <Book bookname={this.state.books[0].bookname} writer={this.state.books[0].writer} />
-        <Book bookname={this.state.books[1].bookname} writer={this.state.books[1].writer} />
-        <Book bookname={this.state.books[2].bookname} writer={this.state.books[2].writer} />
+        <Book bookname={this.state.books[0].bookname} 
+          writer={this.state.books[0].writer} />
+        <Book bookname={this.state.books[1].bookname} 
+          writer={this.state.books[1].writer} />
+        <Book bookname={this.state.books[2].bookname} 
+          writer={this.state.books[2].writer} />
       </div>
     );
   }
