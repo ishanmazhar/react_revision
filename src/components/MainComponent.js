f9f8f007718c9edcb02cbbafaf429225d6f292b9
ishanmazhar <ishanmazhar2@gmail.com>
2021-02-16 01:04:56 +0600

    Importing state from external file

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -1,13 +1,10 @@
 import React, { Component } from 'react'; 
 import Book from './Book'; 
+import bookList from '../assets/books';
 
 class MainComponent extends Component {
     state = {
-        books : [
-          { id: 1, bookname: "1984", writer: "George Orwell"},
-          { id: 2, bookname: "The Da Vinci Code", writer: "Dan Brown"},
-          { id: 3, bookname: "The Alchemist", writer: "Paulo Coelho"}
-        ],
+        books: bookList,
         showBooks: true
       }
     
